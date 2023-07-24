3e319828144e46de26820e61725f077b907dbca6
Christopher Kim's MacBook Pro <christopherkim228@gmail.com>
2023-07-25 03:00:50 +0900

    Finished

---
+++ counter.js
@@ -0,0 +1,55 @@
+var rawExcel;
+var processedJson;
+var currentDate;
+var convertedDate;
+
+// $("#datepicker").datepicker();
+document.getElementById("fileInput").addEventListener("change", readExcel);
+$('#datepicker').datepicker({
+    onSelect: function(dateText) { 
+        console.log("selected"); 
+        currentDate = $('#datepicker').datepicker("getDate"); 
+        convertedDate = 25569.0 + ((currentDate - ((new Date()).getTimezoneOffset() * 60 * 1000)) / (1000 * 60 * 60 * 24));
+        updateNumbers();
+    }
+});
+
+function readExcel(e) {
+    let input = e.target;
+    let reader = new FileReader();
+    reader.onload = function () {
+        let data = reader.result;
+        let workBook = XLSX.read(data, { type: 'binary' });
+        rawExcel = workBook;
+        workBook.SheetNames.forEach(function (sheetName) {
+            console.log('SheetName: ' + sheetName);
+            let rows = XLSX.utils.sheet_to_json(workBook.Sheets[sheetName]);
+            processedJson = rows;
+            console.log(JSON.stringify(rows));
+        })
+    };
+    reader.readAsBinaryString(input.files[0]);
+    updateNumbers();
+}
+
+// let date = new Date();
+// let converted = 25569.0 + ((date.getTime() - (date.getTimezoneOffset() * 60 * 1000)) / (1000 * 60 * 60 * 24));
+
+function updateNumbers() {
+    let korNum = 0;
+    let matNum = 0;
+    let engNum = 0;
+    let c = convertedDate - 45127;
+    for(let i=0; i <= c; i++) {
+        korNum += processedJson[i].국어;
+        matNum += processedJson[i].수학;
+        engNum += processedJson[i].영어;
+        console.log(korNum + ', '+ matNum + ', ' + engNum)
+    }
+    document.getElementById("koreanNumber").innerText = korNum;
+    document.getElementById("mathNumber").innerText = matNum;
+    document.getElementById("englishNumber").innerText = engNum;
+    document.getElementById("koreanDelta").innerText = "+" + processedJson[c].국어;
+    document.getElementById("mathDelta").innerText = "+" + processedJson[c].수학;
+    document.getElementById("englishDelta").innerText = "+" + processedJson[c].영어;
+}
---
+++ webserver.py
@@ -0,0 +1,5 @@
+import http.server
+
+port = 8888
+
+python3 -m http.server 8001 -d /dir_name/
